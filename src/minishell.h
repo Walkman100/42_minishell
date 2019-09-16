8bcfad1e6c891a82e9e694de678505b3630caad3
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-09-16 15:23:21 +0200

    Added start of plan.txt and minishell.h

---
+++ src/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/11 15:43:26 by mcarter           #+#    #+#             */
+/*   Updated: 2019/09/11 15:54:41 by mcarter          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include "../libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>    /* ???? - in man 2 read */
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <signal.h>
+
+#endif
