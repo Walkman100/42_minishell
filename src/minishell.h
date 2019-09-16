27006823ee081a2bd6bb975a21e6e75c6ed79c6a
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-09-16 17:40:17 +0200

    added more info to functions.txt, added some typedefs to minishell.h

--- src/minishell.h
+++ src/minishell.h
@@ -6,13 +6,12 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 15:43:26 by mcarter           #+#    #+#             */
-/*   Updated: 2019/09/11 15:54:41 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/09/16 17:33:11 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-# include "../libft/libft.h"
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
@@ -22,5 +21,9 @@
 # include <sys/stat.h>
 # include <sys/wait.h>
 # include <signal.h>
+# include "../libft/libft.h"
 
+typedef struct stat		t_stat;
+typedef struct dirent	t_dirent;
+typedef struct rusage	t_rusage;
 #endif
