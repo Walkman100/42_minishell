71e1b84971ea4c7078569f2b7bc4dc04104706ab
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-09-25 14:32:07 +0200

    Added echo builtin, fixed cd's prototype

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -6,13 +6,13 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 13:03:31 by mcarter           #+#    #+#             */
-/*   Updated: 2019/09/25 14:15:37 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/09/25 14:27:35 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	builtin_cd(char *args)
+void	builtin_cd(char *args, char **envp)
 {
-	
+
 }
--- src/builtins/builtin_echo.c
+++ src/builtins/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 13:01:02 by mcarter           #+#    #+#             */
-/*   Updated: 2019/09/25 14:15:57 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/09/25 14:31:18 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 
 void	builtin_echo(char *args)
 {
-	
+	char	print_newline;
+
+	print_newline = 1;
+	if (*args == '-')
+	{
+		args++;
+		while (*args && *args != ' ')
+		{
+			if (*args == 'n')
+				print_newline = 0;
+			else
+			{
+				ft_putstr("echo: Invalid option: ");
+				ft_putchar(*args);
+				ft_putchar('\n');
+				return ;
+			}
+			args++;
+		}
+	}
+	while (*args == ' ')
+		args++;
+	ft_putstr(args);
+	if (print_newline)
+		ft_putchar('\n');
 }
