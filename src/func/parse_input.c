785f9f8bd8b4af0e5915e7d1abfc80ceedc131ff
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-09-25 12:39:05 +0200

    Added parse_input code

--- src/func/parse_input.c
+++ src/func/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 12:59:42 by mcarter           #+#    #+#             */
-/*   Updated: 2019/09/24 13:00:25 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/09/25 12:36:34 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,34 @@
 
 t_userinput	parse_input(char *input)
 {
-	t_userinput tmp;
+	t_userinput rtn;
+	MAXUNBR		separator_pos;
 
-	return (tmp);
+	separator_pos = ft_strclen(input, ' ');
+	rtn.program_name = ft_strsub(input, 0, separator_pos);
+	if (ft_strchr(input, ' '))
+		rtn.program_args = input + separator_pos + 1;
+	else
+		rtn.program_args = NULL;
+	if (ft_strequ(rtn.program_name, "echo") ||
+		ft_strequ(rtn.program_name, "exit") ||
+		ft_strequ(rtn.program_name, "clear") ||
+		ft_strequ(rtn.program_name, "cd") ||
+		ft_strequ(rtn.program_name, "pwd") ||
+		ft_strequ(rtn.program_name, "setenv") ||
+		ft_strequ(rtn.program_name, "unsetenv") ||
+		ft_strequ(rtn.program_name, "env"))
+		rtn.is_builtin = 1;
+	else
+		rtn.is_builtin = 0;
+	if (ft_strchr(input, '<'))
+		rtn.redirect_type = 0;
+	else if (ft_strchr(input, '>'))
+	{
+		if (ft_strstr(input, ">>"))
+			rtn.redirect_type = 2;
+		else
+			rtn.redirect_type = 1;
+	}
+	return (rtn);
 }
