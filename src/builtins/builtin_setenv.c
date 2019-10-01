ec7336e5cb93e3261fbafd4384db07a22ae041d4
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-10-01 14:40:12 +0200

    Added setenv and unsetenv
    currently unsetenv doesn't actually remove the variable

--- src/builtins/builtin_setenv.c
+++ src/builtins/builtin_setenv.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 13:04:03 by mcarter           #+#    #+#             */
-/*   Updated: 2019/10/01 12:19:29 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/10/01 14:20:06 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 void	builtin_setenv(char *args, char ***envp)
 {
-	
+	MAXUNBR	splitpos;
+	char	*varname;
+
+if (args && ft_strchr(args, '='))
+	{
+		splitpos = ft_strclen(args, '=') + 1;
+		varname = ft_strsub(args, 0, splitpos);
+		args += splitpos;
+		set_envvar(envp, varname, args);
+		ft_strdel(&varname);
+	}
+	else if (args && ft_strchr(args, ' '))
+	{
+		splitpos = ft_strclen(args, ' ') + 1;
+		varname = ft_strsub(args, 0, splitpos);
+		varname[splitpos - 1] = '=';
+		args += splitpos;
+		set_envvar(envp, varname, args);
+		ft_strdel(&varname);
+	}
+	else
+		ft_putendl("setenv usage:\nsetenv <var> <value>\n OR \nsetenv <var>=<value>");
 }
--- src/builtins/builtin_unsetenv.c
+++ src/builtins/builtin_unsetenv.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 13:04:20 by mcarter           #+#    #+#             */
-/*   Updated: 2019/10/01 12:19:31 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/10/01 14:38:28 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 void	builtin_unsetenv(char *args, char ***envp)
 {
-	
+	MAXUNBR	varnamelen;
+	char	*varname;
+
+	if (args)
+	{
+		varnamelen = ft_strlen(args);
+		varname = ft_strsub(args, 0, varnamelen + 1);
+		varname[varnamelen] = '=';
+		if (get_envvar(*envp, varname))
+		{
+			set_envvar(envp, varname, "");
+			ft_strdel(&varname);
+		}
+		else
+		{
+			ft_putstr("minishell: unsetenv: variable '");
+			ft_putstr(args);
+			ft_putendl("' does not exist");
+		}
+	}
+	else
+		ft_putendl("unsetenv usage: unsetenv <envvar>");
 }
