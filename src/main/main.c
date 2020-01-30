dc7ef007e314fc6f1b02e15e1d800349c16d4aff
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-01-30 14:56:54 +0200

    fixed too many lines in main, made ^D use builtin_exit to exit instead of break

--- src/main/main.c
+++ src/main/main.c
@@ -6,19 +6,34 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 15:49:15 by mcarter           #+#    #+#             */
-/*   Updated: 2019/10/02 13:21:23 by mcarter          ###   ########.fr       */
+/*   Updated: 2020/01/30 14:54:49 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int		main(int argc, char **argv, char **envp)
+void	run_input(char *input, char **path, char ***envp)
 {
-	char		*input;
 	t_userinput	userinput;
-	char		**path;
 	char		*program_path;
 
+	userinput = parse_input(input, envp);
+	if (userinput.is_builtin)
+		run_function(userinput, envp);
+	else if (userinput.program_name && userinput.program_name[0])
+		if ((program_path = get_path(path, userinput.program_name)))
+			wait_for_pid(run_program(program_path, userinput, *envp));
+	ft_strdel(&input);
+	ft_strdel(&(userinput.program_name));
+	ft_strdel(&(userinput.program_args));
+}
+
+int		main(int argc, char **argv, char **envp)
+{
+	char	*input;
+	char	**path;
+
+	argc++;
 	envp_init(&envp, argv);
 	path = get_path_from_envp(envp);
 	while (1)
@@ -26,26 +41,12 @@ int		main(int argc, char **argv, char **envp)
 		show_prompt(envp);
 		if ((input = get_input()))
 		{
-			userinput = parse_input(input, &envp);
-			if (userinput.is_builtin)
-			{
-				run_function(userinput, &envp);
-			}
-			else if (userinput.program_name && userinput.program_name[0])
-			{
-				if ((program_path = get_path(path, userinput.program_name)))
-					wait_for_pid(run_program(program_path, userinput, envp));
-			}
-			ft_strdel(&input);
-			ft_strdel(&(userinput.program_name));
-			ft_strdel(&(userinput.program_args));
+			run_input(input, path, &envp);
 		}
 		else
 		{
 			ft_putstr("exit\n");
-			free_envp(&envp);
-			break ;
+			builtin_exit(NULL, &envp);
 		}
 	}
-	return (0);
 }
